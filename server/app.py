64676af102d527be987999b7de45824ff46cef17
Joseph Smith <joesmith40@gmail.com>
2023-06-21 13:22:41 -0500

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, abort
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = True
 
 migrate = Migrate(app, db)
@@ -16,12 +16,48 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(jsonify(plants_list), 200)
+        return response
+    
+    def post(self):
+        request_json = request.get_json()
+
+        new_plant = Plant(
+            name = request_json['name'],
+            image = request_json['image'],
+            price = request_json['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(new_plant.to_dict()),
+            201
+        )
+        return response
+
+
+api.add_resource(Plants, "/plants")
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            abort(404, 'The plant you were looking for was not found!')
+        response = make_response(
+            jsonify(plant.to_dict()),
+            200
+        )
+        return response
 
-if __name__ == '__main__':
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
